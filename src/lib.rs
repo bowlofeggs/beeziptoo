ab0cfef46737600370cf33bee598c839b9c02935
Randy Barlow <randy@electronsweatshop.com>
2023-10-16 17:41:40 -0400

    Rename Error to DecompressError
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,18 @@ use thiserror::Error as ThisError;
 
 mod rle;
 
-/// These are the possible errors that can occur during operation.
+/// These are the possible errors that can occur during decompression.
 #[derive(Debug, ThisError)]
-pub enum Error {
+pub enum DecompressError {
     /// The runlength decoder encountered an invalid input.
     #[error("Failed to decode at a runlength step")]
     RunLengthDecode,
 }
 
-impl From<rle::Error> for Error {
+impl From<rle::Error> for DecompressError {
     fn from(value: rle::Error) -> Self {
         match value {
-            rle::Error::RunLengthTruncated => Error::RunLengthDecode,
+            rle::Error::RunLengthTruncated => DecompressError::RunLengthDecode,
         }
     }
 }
@@ -31,6 +31,6 @@ pub fn compress(data: &[u8]) -> Vec<u8> {
 /// # Errors
 ///
 /// This function is failable since it is possible the given data isn't a valid `bzip2` archive.
-pub fn decompress(data: &[u8]) -> Result<Vec<u8>, Error> {
+pub fn decompress(data: &[u8]) -> Result<Vec<u8>, DecompressError> {
     rle::reverse(data).map_err(|e| e.into())
 }
