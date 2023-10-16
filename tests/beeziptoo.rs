f6c0c1704370eb63478f08411f26eececf288708
Randy Barlow <randy@electronsweatshop.com>
2023-10-16 18:10:30 -0400

    Add a round trip integration test
    This asserts that compressing and decompressing data results in the
    original data.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

---
+++ tests/beeziptoo.rs
@@ -0,0 +1,19 @@
+//! Test the beeziptoo public interface.
+use std::io::Read;
+
+use beeziptoo::{compress, decompress};
+
+/// Assert that a roundtrip of compression and decompression results in the same data.
+#[test]
+fn roundtrip() {
+    let data = b"ABBCCCDDDDEEEEEFFFFFFGGGGGGGHHHHHHHH";
+
+    let mut compressed_data = compress(&data[..]).expect("Could not compress data");
+    let mut result = decompress(&mut compressed_data).expect("Could not decompress data");
+
+    let mut all_data = vec![];
+    result
+        .read_to_end(&mut all_data)
+        .expect("Could not read decompressed data");
+    assert_eq!(all_data, data);
+}
