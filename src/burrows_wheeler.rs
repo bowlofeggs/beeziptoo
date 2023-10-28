7f3a8c9a153e7cf24bb367db84e2ef33f496f04e
Andrew Halle <me@andrewhalle.dev>
2023-10-27 20:30:59 -0700

    Write `encode()`

--- src/burrows_wheeler.rs
+++ src/burrows_wheeler.rs
@@ -1,7 +1,35 @@
 use std::collections::VecDeque;
 
+/// Encode with the Burrows-Wheeler Transform.
+///
+/// # Notes
+///
+/// The origin pointer (index of original row in the sorted rotation block) is appended to the end of the
+/// data as 3 little-endian bytes.
 pub(super) fn encode(data: &[u8]) -> Vec<u8> {
-    todo!()
+    // ASSUMPTION max block size for this stage is 900 kB
+    assert!(data.len() < 900_000);
+
+    if data.is_empty() {
+        return Vec::new();
+    }
+
+    let mut all_rotations = all_rotations(data);
+    all_rotations.sort_unstable();
+    let origin_pointer = all_rotations
+        .binary_search_by_key(&data, |v| &v[..])
+        .expect("`data` must be in `all_rotations`");
+    let mut output: Vec<u8> = all_rotations
+        .into_iter()
+        .map(|mut v| {
+            v.pop()
+                .expect("presence of inner `Vec` means data was not empty")
+        })
+        .collect();
+    // ASSUMPTION we're encoding the origin pointer as a little-endian index.
+    output.extend_from_slice(&origin_pointer.to_le_bytes()[..3]);
+
+    output
 }
 
 pub(super) fn decode(data: &[u8]) -> Vec<u8> {
@@ -21,6 +49,27 @@ fn all_rotations(data: &[u8]) -> Vec<Vec<u8>> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    mod encode {
+        use super::*;
+
+        #[test]
+        fn small() {
+            let input = b"cdab";
+
+            let encoded = encode(input);
+
+            assert_eq!(&encoded[..4], b"dabc");
+            assert_eq!(&encoded[4..], &[2, 0, 0]);
+        }
+
+        #[test]
+        fn empty() {
+            let encoded = encode(&[]);
+
+            assert_eq!(encoded, &[]);
+        }
+    }
 
     mod all_rotations {
         use super::*;
